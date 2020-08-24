4110a91df798f7cd4a74d74cfd8afccdac6df426
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-08-24 19:29:03 +0300

    removed legacy

--- src/VPX.Web/ClientApp/src/app/components/lecture-components/lectures-page/lectures-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-lectures-page',
-  templateUrl: './lectures-page.component.html',
-  styleUrls: ['./lectures-page.component.css']
-})
-export class LecturesPageComponent { }
--- src/VPX.Web/ClientApp/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiEndpoint: 'https://localhost:44306/api/'
-};
